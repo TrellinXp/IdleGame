b6de7ed0d2add32c40ac0f55b919bb0e82206957
Peter Mayer <Peter_My@gmx.de>
2025-08-17 17:26:02 +0200

    Dinosaurs initial Layout

--- IdleGame/src/app/dinosaurs/dinosaurs.ts
+++ IdleGame/src/app/dinosaurs/dinosaurs.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
-
+import { NgFor} from '@angular/common'; 
+import firebase from 'firebase/compat/app';
+import { environment } from '../environments/environment';
+import { collection, doc, getDocs, getFirestore, updateDoc } from "firebase/firestore";
 @Component({
   selector: 'app-dinosaurs',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './dinosaurs.html',
   styleUrl: './dinosaurs.scss'
 })
 export class Dinosaurs {
+  dinosaurs: any[] = [];
 
+  constructor() {
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+      firebase.initializeApp(environment.firebase);
+    } else {
+      firebase.app(); // if already initialized, use that one
+      console.log("Firebase already initialized");
+    }
+
+    this.fetchDinosaurs();
+    console.log("Dinosaurs component initialized");
+  }
+
+  async fetchDinosaurs(): Promise<void> {
+    const db = getFirestore(firebase.app());
+    const querySnapshot = await getDocs(collection(db, "dinosaurs"));
+    querySnapshot.forEach((doc) => {
+      this.dinosaurs.push(doc.data());
+    });
+    console.log("Finished fetching dinosaurs from Firestore")
+    this.dinosaurs.sort((a, b) => a.name.localeCompare(b.name));  
+  }
 }
+
+
---
+++ IdleGame/src/app/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyAR7WuC11Sm5Hp8bS3C71rqBR2s6qF8u-M",
+        authDomain: "idle-dinosaur-angular.firebaseapp.com",
+        projectId: "idle-dinosaur-angular",
+        storageBucket: "idle-dinosaur-angular.firebasestorage.app",
+        messagingSenderId: "732585017361",
+        appId: "1:732585017361:web:de5da654499d2b8a3931b0",
+        measurementId: "G-TTHE1QFCXJ"
+      },
+};
